3df08f7050b6bb634d768524b7d87283cfcec07d
Anele Danisa <aneledanisa10@gmail.com>
2022-07-12 19:40:57 -0800

    Taking the file user input.

---
+++ src/Get_The_Sum.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Get_The_Sum {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        int target = input.nextInt();
+        String empty = input.nextLine();
+        System.out.println("Are you going tyo use the file or numbers");
+        System.out.println("Enter n for numbers or f for file");
+        String question = input.nextLine();
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        if(question.equalsIgnoreCase("f")){
+            System.out.println("Enter the name of a file");
+            String filename = input.nextLine();
+            File file = new File("src/" +filename);
+            Scanner line_Scanner = new Scanner(file);
+
+            while(line_Scanner.hasNext()){
+                nums.add(Integer.parseInt(line_Scanner.nextLine()));
+            }
+
+        }
+/*        else if (question.equalsIgnoreCase("n")) {
+
+        }*/
+    }
+}
